ceacb9cf09e0f8c4b8f181f8af3394940e12ff33
Jader <jader.martins@list.lu>
2023-04-24 10:31:21 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Experiments on Embeddings
+
+
+Yes, but Did It Work?: Evaluating Variational Inference
+https://arxiv.org/pdf/1802.02538.pdf
+
+Distance Assessment and Hypothesis Testing of High-Dimensional Samples using Variational Autoencoders
+https://arxiv.org/pdf/1909.07182.pdf
+
+A Recurrent Latent Variable Model for Sequential Data
+https://arxiv.org/pdf/1506.02216.pdf
--- src/model.py
+++ src/model.py
@@ -1,6 +1,62 @@
 import torch
+from typing import Optional
 import torch.nn as nn
 import torch.nn.functional as F
+from transformers.models.bert.modeling_bert import BertEmbeddings
+
+
+class VBertEmbeddings(BertEmbeddings):
+    def __init__(self, config):
+        super().__init__(config)
+        self.dev_word_embeddings = nn.Embedding(
+                config.vocab_size,
+                config.hidden_size,
+                padding_idx=config.pad_token_id)
+
+    def forward(
+        self,
+        input_ids: Optional[torch.LongTensor] = None,
+        token_type_ids: Optional[torch.LongTensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        past_key_values_length: int = 0,
+    ) -> torch.Tensor:
+        if input_ids is not None:
+            input_shape = input_ids.size()
+        else:
+            input_shape = inputs_embeds.size()[:-1]
+
+        seq_length = input_shape[1]
+
+        if position_ids is None:
+            position_ids = self.position_ids[:, past_key_values_length : seq_length + past_key_values_length]
+
+        # Setting the token_type_ids to the registered buffer in constructor where it is all zeros, which usually occurs
+        # when its auto-generated, registered buffer helps users when tracing the model without passing token_type_ids, solves
+        # issue #5664
+        if token_type_ids is None:
+            if hasattr(self, "token_type_ids"):
+                buffered_token_type_ids = self.token_type_ids[:, :seq_length]
+                buffered_token_type_ids_expanded = buffered_token_type_ids.expand(input_shape[0], seq_length)
+                token_type_ids = buffered_token_type_ids_expanded
+            else:
+                token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=self.position_ids.device)
+
+        if inputs_embeds is None:
+            mu = self.word_embeddings(input_ids)
+            sigma = self.dev_word_embeddings(input_ids)
+            eps = torch.randn_like(sigma)
+            inputs_embeds = mu + eps * sigma
+
+        token_type_embeddings = self.token_type_embeddings(token_type_ids)
+
+        embeddings = inputs_embeds + token_type_embeddings
+        if self.position_embedding_type == "absolute":
+            position_embeddings = self.position_embeddings(position_ids)
+            embeddings += position_embeddings
+        embeddings = self.LayerNorm(embeddings)
+        embeddings = self.dropout(embeddings)
+        return embeddings, mu, sigma
 
 
 class Encoder(nn.Module):
