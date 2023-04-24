4ad412eeb934b1e7983220984266516938e1f6ee
Jader <jader.martins@list.lu>
2023-04-24 17:33:42 +0200

    update

--- src/main.py
+++ src/main.py
@@ -124,7 +124,7 @@ def kl_criterion(output, targets, mu, logvar):
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    KLD = torch.log(-0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()))
 
     return NLL + KLD
 ###############################################################################
