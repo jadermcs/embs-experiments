e7709dd2f5af49cf44b418e7d4fcd4cfa46f8172
Jader <jader.martins@list.lu>
2024-07-11 11:10:01 +0200

    update

--- loratrain.py
+++ loratrain.py
@@ -1,106 +1,133 @@
 import numpy as np
-from peft import LoraConfig, TaskType
-from peft import get_peft_model
-from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainer, Seq2SeqTrainingArguments
-from transformers import AutoTokenizer, DataCollatorForSeq2Seq
-from datasets import load_dataset
+from datasets import load_dataset, concatenate_datasets
+from peft import LoraConfig, get_peft_model, TaskType
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import DataCollatorForSeq2Seq
+from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
 
-
-model = AutoModelForSeq2SeqLM.from_pretrained("google/mt5-base")
-print(model)
-tokenizer = AutoTokenizer.from_pretrained("google/mt5-base")
+# Load dataset
 
 paths = {x: f"data/dimension.{x}.csv" for x in ("train", "valid", "test")}
 dataset = load_dataset("csv", data_files=paths, delimiter="\t")
 
-
-def preprocess_function(example):
-    label = "Identesch" if example['label'] == "identical" else "Ënnerschiddlech"
-    item = {
-            "prompt": f"{example['sentence1']}\r\n{example['sentence2']}",
-            "answer": f"Ass eng Bedeitung vu '{example['lemma']}' identesch oder ënnerschiddlech? {label}",
-            }
-    item["len_prompt"] = len(tokenizer(item["prompt"]).input_ids)
-    item["len_answer"] = len(tokenizer(item["answer"]).input_ids)
-    return item
+print(f"Train dataset size: {len(dataset['train'])}")
+print(f"Validation dataset size: {len(dataset['valid'])}")
 
 
-dataset = dataset.map(
-    preprocess_function,
-    num_proc=4,
-    remove_columns=dataset["train"].column_names,
-)
-print(dataset)
+# Set model and load tokenizer
+model_id = "google/mt5-small"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-max_source = int(np.percentile(dataset["train"]["len_prompt"], 99))
-print(max_source)
-max_target = int(np.percentile(dataset["train"]["len_answer"], 99))
-print(max_target)
+# The maximum total input sequence length after tokenization.
+# Sequences longer than this will be truncated, sequences shorter will be padded.
+tokenized_inputs = concatenate_datasets(
+        [dataset["train"], dataset["valid"]]
+        ).map(lambda x: tokenizer(x["prompt"], truncation=True),
+              batched=True,
+              remove_columns=dataset["train"].column_names)
+input_lenghts = [len(x) for x in tokenized_inputs["input_ids"]]
+# take 85 percentile of max length for better utilization
+max_source_length = int(np.percentile(input_lenghts, 99))
+print(f"Max source length: {max_source_length}")
+
+# The maximum total sequence length for target text after tokenization.
+# Sequences longer than this will be truncated, sequences shorter will be padded."
+tokenized_targets = concatenate_datasets(
+        [dataset["train"], dataset["valid"]]
+        ).map(lambda x: tokenizer(x["answer"], truncation=True),
+              batched=True,
+              remove_columns=dataset["train"].column_names)
+target_lenghts = [len(x) for x in tokenized_targets["input_ids"]]
+# take 90 percentile of max length for better utilization
+max_target_length = int(np.percentile(target_lenghts, 99))
+print(f"Max target length: {max_target_length}")
 
 
-def tokenize_function(example):
-    item = tokenizer(
-            example["prompt"],
-            max_length=max_source,
-            padding="max_length", truncation=True)
-    labels = tokenizer(
-            text_target=example["answer"],
-            max_length=max_target,
-            padding="max_length", truncation=True).input_ids
-    labels = [
-            [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels
+def preprocess_function(sample, padding="max_length"):
+    # add prefix to the input for t5
+    inputs = ["Sätz:\r\n" + item for item in sample["prompt"]]
+
+    # tokenize inputs
+    model_inputs = tokenizer(inputs,
+                             max_length=max_source_length,
+                             padding=padding,
+                             truncation=True)
+
+    # Tokenize targets with the `text_target` keyword argument
+    labels = tokenizer(text_target=sample["answer"],
+                       max_length=max_target_length,
+                       padding=padding,
+                       truncation=True)
+
+    # If we are padding here, replace all tokenizer.pad_token_id in the labels
+    # by -100 when we want to ignore padding in the loss.
+    if padding == "max_length":
+        labels["input_ids"] = [
+            [(l if l != tokenizer.pad_token_id else -100) for l in label] \
+                    for label in labels["input_ids"]
         ]
 
-    item["labels"] = labels
-    return item
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
 
 
-dataset = dataset.map(
-    tokenize_function,
-    num_proc=4,
-    batched=True,
-    remove_columns=dataset["train"].column_names,
+tokenized_dataset = dataset.map(preprocess_function,
+                                batched=True,
+                                remove_columns=dataset["train"].column_names)
+print(f"Keys of tokenized dataset: {list(tokenized_dataset['train'].features)}")
+
+# save datasets to disk for later easy loading
+tokenized_dataset["train"].save_to_disk("data/train")
+tokenized_dataset["valid"].save_to_disk("data/eval")
+
+
+# load model from the hub
+model = AutoModelForSeq2SeqLM.from_pretrained(model_id, device_map="auto")
+# Define LoRA Config
+lora_config = LoraConfig(
+ r=16,
+ lora_alpha=32,
+ target_modules=["q", "v"],
+ lora_dropout=0.05,
+ bias="none",
+ task_type=TaskType.SEQ_2_SEQ_LM
 )
-print(dataset)
-print(dataset["train"][0])
-
-peft_config = LoraConfig(
-        task_type=TaskType.SEQ_2_SEQ_LM,
-        target_modules=["q", "v"],
-        r=8,
-        lora_alpha=32,
-        lora_dropout=0.1)
-
-model = get_peft_model(model, peft_config)
+# add LoRA adaptor
+model = get_peft_model(model, lora_config)
 model.print_trainable_parameters()
 
+# we want to ignore tokenizer pad token in the loss
+label_pad_token_id = -100
 # Data collator
 data_collator = DataCollatorForSeq2Seq(
     tokenizer,
     model=model,
-    label_pad_token_id=-100,
+    label_pad_token_id=label_pad_token_id,
     pad_to_multiple_of=8
 )
 
+output_dir = "lora-mt5"
 
+# Define training args
 training_args = Seq2SeqTrainingArguments(
-    output_dir="LIST/ltz-mt5-lora",
-    learning_rate=1e-3,
+    output_dir=output_dir,
     auto_find_batch_size=True,
-    num_train_epochs=2,
-    weight_decay=0.01,
-    eval_strategy="epoch",
-    save_strategy="epoch",
-    load_best_model_at_end=True,
+    learning_rate=1e-3,
+    num_train_epochs=5,
+    logging_dir=f"{output_dir}/logs",
+    logging_strategy="steps",
+    logging_steps=500,
+    save_strategy="no",
 )
 
+# Create Trainer instance
 trainer = Seq2SeqTrainer(
     model=model,
     args=training_args,
-    train_dataset=dataset["train"],
-    eval_dataset=dataset["valid"],
-    tokenizer=tokenizer,
     data_collator=data_collator,
+    train_dataset=tokenized_dataset["train"],
 )
+model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
 
+# train model
 trainer.train()
